50b796268a007990542d7810840490cb5c9fbdfb
moobaaclub <109789290+moobaaclub@users.noreply.github.com>
2022-12-02 16:27:46 +1100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-npm install -D @types/rngstudio/flip
+npm install -D @types/rngstudio
 import { NextPage } from "next";
 import Head from "next/head";
 import { Header } from "../components/navigation/Header";
